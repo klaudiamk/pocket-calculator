d0e42f1e183797a1d990c95ab06f41a19981892c
Klaudia <klaudiak9019@gmail.com>
2019-01-02 19:38:28 -0500

    design

---
+++ pocket-calculator.js
@@ -0,0 +1,51 @@
+function zero() {
+
+}
+function one() {
+
+}
+function two() {
+
+}
+function three() {
+
+}
+function four() {
+
+}
+function five() {
+
+}
+function six() {
+
+}
+function seven() {
+
+}
+function eight() {
+
+}
+function nine() {
+
+}
+function equals() {
+
+}
+function division() {
+
+}
+function multiplication() {
+
+}
+function subtraction() {
+
+}
+function addition() {
+
+}
+function percent() {
+
+}
+function plusMinus() {
+
+}
