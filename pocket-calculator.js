2cf95077b4a9b4ab9a0688c759f9f8eeaa0cd012
Klaudia Kedra <klaudiak9019@gmail.com>
2019-01-04 10:47:10 -0500

    added display screen

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,32 +1,61 @@
-function zero() {
+let p = document.getElementById("display");
+let str = ""
+p.innerHTML = 0;
 
+function zero() {
+  let p = document.getElementById("display");
+  str += "0";
+  p.innerHTML = str;
 }
 function one() {
-
+let p = document.getElementById("display");
+str += "1";
+p.innerHTML = str;
 }
 function two() {
-
+  let p = document.getElementById("display");
+  str += "2";
+  p.innerHTML = str;
 }
 function three() {
-
+  let p = document.getElementById("display");
+  str += "3";
+  p.innerHTML = str;
 }
 function four() {
-
+  let p = document.getElementById("display");
+  str += "4";
+  p.innerHTML = str;
 }
 function five() {
-
+  let p = document.getElementById("display");
+  str += "5";
+  p.innerHTML = str;
 }
 function six() {
-
+  let p = document.getElementById("display");
+  str += "6";
+  p.innerHTML = str;
 }
 function seven() {
-
+  let p = document.getElementById("display");
+  str += "7";
+  p.innerHTML = str;
 }
 function eight() {
-
+  let p = document.getElementById("display");
+  str += "8";
+  p.innerHTML = str;
 }
 function nine() {
-
+  let p = document.getElementById("display");
+  str += "9";
+  p.innerHTML = str;
+}
+function decimal() {
+  let p = document.getElementById("display");
+  str += ".";
+  p.innerHTML = str;
 }
 function equals() {
 
@@ -48,4 +77,7 @@ function percent() {
 }
 function plusMinus() {
 
+}
+function clear() {
+  
 }
