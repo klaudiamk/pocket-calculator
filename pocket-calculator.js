5f279b02257a69bdeb73db80d52f8a2391eb5fc4
Klaudia <klaudiak9019@gmail.com>
2019-01-11 18:39:49 -0500

    nothing works

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,100 +1,97 @@
-let p = document.getElementById("display");
-let str = ""
-p.innerHTML = 0;
+function dot(val){
 
-function zero() {
-  let p = document.getElementById("display");
-  str += "0";
-  p.innerHTML = str;
-}
-function one() {
-let p = document.getElementById("display");
-str += "1";
-p.innerHTML = str;
-}
-function two() {
-  let p = document.getElementById("display");
-  str += "2";
-  p.innerHTML = str;
-}
-function three() {
-  let p = document.getElementById("display");
-  str += "3";
-  p.innerHTML = str;
-}
-function four() {
-  let p = document.getElementById("display");
-  str += "4";
-  p.innerHTML = str;
-}
-function five() {
-  let p = document.getElementById("display");
-  str += "5";
-  p.innerHTML = str;
-}
-function six() {
-  let p = document.getElementById("display");
-  str += "6";
-  p.innerHTML = str;
-}
-function seven() {
-  let p = document.getElementById("display");
-  str += "7";
-  p.innerHTML = str;
-}
-function eight() {
-  let p = document.getElementById("display");
-  str += "8";
-  p.innerHTML = str;
-}
-function nine() {
-  let p = document.getElementById("display");
-  str += "9";
-  p.innerHTML = str;
-}
-function decimal() {
-  let p = document.getElementById("display");
-  str += ".";
-  p.innerHTML = str;
-}
-function equals() {
-  let p = document.getElementById("display");
-  p.innerHTML = str;
-}
-function division() {
-  let p = document.getElementById("display");
-  str += " / ";
-  p.innerHTML = str;
-}
-function multiplication() {
-  let p = document.getElementById("display");
-  str += " * ";
-  p.innerHTML = str;
-}
-function subtraction() {
-  let p = document.getElementById("display");
-  str += " - ";
-  p.innerHTML = str;
-}
-function addition() {
-  let p = document.getElementById("display");
-  str += " + ";
-  p.innerHTML = str;
+//placeholder keeps the zero after the decimal point
+
+//do later (decimal point)
+
+document.getElementById("result").value+=val+0;
+
+
+
 }
 
-function percent() {
-  let p = document.getElementById("display");
-  str += " % ";
-  p.innerHTML = str;
+function dis(val)  {
+
+       document.getElementById("result").value+=val
+
 }
 
-function plusMinus() {
-  let p = document.getElementById("display");
-  str += "-";
-  p.innerHTML = str;
+
+
+
+
+function clr() {
+
+    document.getElementById("result").value =" "
+
+  }
+
+
+
+function per(val) {
+
+  let x = document.getElementById("result").value
+
+  let y = eval(x)/100;
+
+ document.getElementById("result").value = y
+
+ if (isNaN(y) ) {
+
+  document.getElementById("result").value = "Error";
+
 }
-function clear() {
-  let p = document.getElementById("display");
-  str += "";
-  p.innerHTML = str;
+
+/*thousands place commas*/
+
+if (y > 999){
+
+  document.getElementById("result").value = y.toLocaleString();
+
+}
+
+/*scientific notation */
+
+if (y > 999999999){
+
+    document.getElementById("result").value = y.toExponential();
+
+}
+
+}
+
+
+
+function solve() {
+
+  let x = document.getElementById("result").value
+
+  let y = eval(x);
+
+ document.getElementById("result").value = y
+
+/*error message*/
+
+ if (isNaN(y) ) {
+
+  document.getElementById("result").value = "Error";
+
+}
+
+/*thousands place commas*/
+
+if (y > 999){
+
+  document.getElementById("result").value = y.toLocaleString();
+
+}
+
+/*scientific notation */
+
+if (y > 999999999){
+
+    document.getElementById("result").value = y.toExponential();
+
+}
+
 }
