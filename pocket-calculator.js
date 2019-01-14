aee2e7e57c0e420a52a8039f8bf630ebc29572d6
Klaudia Kedra <klaudiak9019@gmail.com>
2019-01-14 10:51:56 -0500

    fixed buttons; change display

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,22 +1,64 @@
-function solve() {
-  let x = document.getElementById("display").value
-  let y = eval(x);
+let p = document.getElementById("display");
+let str = "";
+p.innerHTML = 0;
 
- document.getElementById("display").value = y
-
-/*error message*/
- if (isNaN(y) ) {
-  document.getElementById("display").value = "Error";
- }
-
-/*thousands place commas*/
-if (y > 999){
-  document.getElementById("display").value = y.toLocaleString();
+function one() {
+  let p = document.getElementById("display");
+  str += "1";
+  p.innerHTML = str;
 }
-
-/*scientific notation */
-if (y > 999999999){
-    document.getElementById("display").value = y.toExponential();
+function two() {
+  let p = document.getElementById("display");
+  str += "2";
+  p.innerHTML = str;
 }
-
+function three() {
+  let p = document.getElementById("display");
+  str += "3";
+  p.innerHTML = str;
+}
+function four() {
+  let p = document.getElementById("display");
+  str += "4";
+  p.innerHTML = str;
+}
+function five() {
+  let p = document.getElementById("display");
+  str += "5";
+  p.innerHTML = str;
+}
+function six() {
+  let p = document.getElementById("display");
+  str += "6";
+  p.innerHTML = str;
+}
+function seven() {
+  let p = document.getElementById("display");
+  str += "7";
+  p.innerHTML = str;
+}
+function eight() {
+  let p = document.getElementById("display");
+  str += "8";
+  p.innerHTML = str;
+}
+function nine() {
+  let p = document.getElementById("display");
+  str += "9";
+  p.innerHTML = str;
+}
+function zero() {
+  let p = document.getElementById("display");
+  str += "0";
+  p.innerHTML = str;
+}
+function addition() {
+  let p = document.getElementById("display");
+  str += "+";
+  p.innerHTML = str;
+}
+function subtraction() {
+  let p = document.getElementById("display");
+  str += "-";
+  p.innerHTML = str;
 }
