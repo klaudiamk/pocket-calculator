33e51892a387c495ddfcff6b5e892d92efece79c
Klaudia K <43784749+klaudiamk@users.noreply.github.com>
2019-01-14 08:34:03 -0500

    changed js

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,88 +1,20 @@
-function dot(val){
-
-//placeholder keeps the zero after the decimal point
-
-//do later (decimal point)
-
-document.getElementById("result").value+=val+0;
-
-
-
-}
-
-function dis(val)  {
-
-       document.getElementById("result").value+=val
-
-}
-
-
-
-
-
-function clr() {
-
-    document.getElementById("result").value =" "
-
-  }
-
-
-
-function per(val) {
-
-  let x = document.getElementById("result").value
-
-  let y = eval(x)/100;
-
- document.getElementById("result").value = y
-
- if (isNaN(y) ) {
-
-  document.getElementById("result").value = "Error";
-
-}
-
-/*thousands place commas*/
-
-if (y > 999){
-
-  document.getElementById("result").value = y.toLocaleString();
-
-}
-
-/*scientific notation */
-
-if (y > 999999999){
-
-    document.getElementById("result").value = y.toExponential();
-
-}
-
-}
-
-
-
 function solve() {
-
-  let x = document.getElementById("result").value
-
+  let x = document.getElementById("display").value
   let y = eval(x);
 
- document.getElementById("result").value = y
+ document.getElementById("display").value = y
 
 /*error message*/
 
  if (isNaN(y) ) {
-
-  document.getElementById("result").value = "Error";
-
-}
+  document.getElementById("display").value = "Error";
+ }
 
 /*thousands place commas*/
 
 if (y > 999){
 
-  document.getElementById("result").value = y.toLocaleString();
+  document.getElementById("display").value = y.toLocaleString();
 
 }
 
@@ -90,7 +22,7 @@ if (y > 999){
 
 if (y > 999999999){
 
-    document.getElementById("result").value = y.toExponential();
+    document.getElementById("display").value = y.toExponential();
 
 }
 
